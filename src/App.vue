adf82305dfe80b6e9261c2d140126bab9570e799
Lin-hwai-chih <tony789520@gmail.com>
2026-09-11 00:10:20 +0800

    vue基本網站建置

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+console.log("我的第一個組件");
+</script>
+
+<template>
+  <nav>基本的導覽列</nav>
+  <main>基本的主內容</main>
+</template>
+
+<style scoped>
+main {
+  background-color: #dddddd;
+}
+</style>

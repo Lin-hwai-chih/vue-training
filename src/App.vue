e432f60525d24975801ae23264d5bfc4890fadcb
Lin-hwai-chih <tony789520@gmail.com>
2026-09-26 16:05:00 +0800

    Fetch API串接後端資料-前端工程框架

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <script setup>
-import { ref } from "vue";
-import MainComp from "./Main.vue";
-let visible = ref(true);
-let hide = function () {
-  visible.value = false;
-};
-let show = function () {
-  visible.value = true;
-};
+//後端提供的資料串接網址：https://cwpeng.github.io/live-records-samples/data/products.json
+import { onMounted, ref } from "vue";
+import List from "./List.vue";
+let products = ref(null); //建立響應式狀態products，一開始是空值null
+//組件掛載完成後，開始呼叫fetch串接後端的資料
+onMounted(async function () {
+  let respones = await fetch(
+    "https://cwpeng.github.io/live-records-samples/data/products.json"
+  );
+  let data = await respones.json();
+  console.log(data);
+  products.value = data;
+});
 </script>
 
 <template>
-  <MainComp v-if="visible"></MainComp>
-  <button @click="hide">隱藏</button>
-  <button @click="show">重新顯示</button>
+  <div class="headline">產品資料列表</div>
+  <div v-if="products === null">資料載入中</div>
+  <!-- <div v-else>資料已載入</div> -->
+  <List v-else :products="products"></List>
 </template>
  
 <style scoped>
+.headline {
+  font-size: 20px;
+  font-weight: bold;
+}
 </style>
---
+++ src/App_8.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue";
+import MainComp from "./Main.vue";
+let visible = ref(true);
+let hide = function () {
+  visible.value = false;
+};
+let show = function () {
+  visible.value = true;
+};
+</script>
+
+<template>
+  <MainComp v-if="visible"></MainComp>
+  <button @click="hide">隱藏</button>
+  <button @click="show">重新顯示</button>
+</template>
+ 
+<style scoped>
+</style>
